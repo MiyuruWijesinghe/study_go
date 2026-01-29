cd6b701db7bb4a96ffbce1cbc5d23a96791d43e1
MiyuruWijesinghe <mkwijesinghe2016@gmail.com>
2026-01-29 23:13:11 +0530

    for loop

---
+++ forLoop.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*i := 1
+
+	for i <= 5 {
+		fmt.Println("Animal ", i)
+		i++
+	}*/ //basic one
+
+	//shortest one
+
+	for i := 1; i <= 5; i++ {
+		fmt.Println("Animal ", i)
+	}
+}
